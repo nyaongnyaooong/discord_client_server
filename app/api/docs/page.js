8728c4145334293f896f6ec24233e49182ca4729
nyaongnyaooong <luckyyou123@naver.com>
2023-11-17 16:43:29 +0900

    swagger

---
+++ app/api/docs/page.js
@@ -0,0 +1,33 @@
+'use client'
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const Main = () => {
+  const router = useRouter();
+
+  const { protocol, hostname } = window.location;
+  const serverUrl = process.env.NEXT_PUBLIC_SERVER_PORT ?
+    `${protocol}//${hostname}:${process.env.NEXT_PUBLIC_SERVER_PORT}` :
+    "http://localhost:3040";
+
+  const apiDocsUrl = serverUrl + '/api/docs';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(serverUrl + '/api/docs');
+      const docs = response.data;
+    };
+    fetchData();
+  }, [])
+
+  router.push(apiDocsUrl);
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default Main;
